d6cb8184f57c5e5b1064e872a7273c418e4b6e5e
Heval Berk Nevruz <hevalberknevruz@gmail.com>
2015-03-18 00:42:16 +0200

    Spring REST Service

---
+++ src/main/java/com/springapp/mvc/controller/CustomerRestController.java
@@ -0,0 +1,32 @@
+package com.springapp.mvc.controller;
+
+import com.springapp.mvc.model.Customer;
+import com.springapp.mvc.service.ICustomerService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+/**
+ * Created by heval-Computer on 18.3.2015.
+ */
+@RestController
+@RequestMapping(value = "/rest")
+public class CustomerRestController {
+
+    @Autowired
+    ICustomerService service;
+
+    @RequestMapping(value = "/customer/{id}", method = RequestMethod.GET)
+    public @ResponseBody Customer getCustomer(@PathVariable int id){
+        Customer customer=service.getCustomer(id);
+        return customer;
+    }
+
+    @RequestMapping(value = "customer/list", method = RequestMethod.GET)
+    public @ResponseBody List<Customer> getCustomerList(){
+        List<Customer> customerList=service.getCustomerList();
+        return customerList;
+    }
+
+}
