5ecf9c780db7265bd2f702748295c807020bd007
Heval Berk Nevruz <hevalberknevruz@gmail.com>
2015-03-18 22:05:55 +0200

    @Transactional Regulation

--- src/main/java/com/springapp/mvc/service/CustomerService.java
+++ src/main/java/com/springapp/mvc/service/CustomerService.java
@@ -4,6 +4,7 @@ import com.springapp.mvc.dao.ICustomerDAO;
 import com.springapp.mvc.model.Customer;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
@@ -12,7 +13,8 @@ import java.util.List;
  * Created by heval-Computer on 16.3.2015.
  */
 
-@Transactional
+/* Sadece commit edilmiş verilerin okunması için */
+@Transactional(timeout = 20,isolation = Isolation.READ_COMMITTED)
 @Service
 public class CustomerService implements ICustomerService{
 
@@ -20,8 +22,9 @@ public class CustomerService implements ICustomerService{
     private ICustomerDAO dao;
 
     @Override
-    public void insertOrUpdateCustomer(Customer customer) {
+    public Customer insertOrUpdateCustomer(Customer customer) {
         dao.insertOrUpdateCustomer(customer);
+        return customer;
     }
 
     @Override
@@ -29,12 +32,14 @@ public class CustomerService implements ICustomerService{
         dao.delete(id);
     }
 
+    @Transactional(readOnly = true)
     @Override
     public List<Customer> getCustomerList() {
         List<Customer> customerList=dao.getCustomerList();
         return customerList;
     }
 
+    @Transactional(readOnly = true )
     @Override
     public Customer getCustomer(int id) {
         Customer customer=dao.getCustomer(id);
